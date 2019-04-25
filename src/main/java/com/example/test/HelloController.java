aa45ab8b4534f379c6a02f6936c7d311a6426197
htwhn <49970923+htwhn@users.noreply.github.com>
2019-04-25 14:16:48 +0800

    Initial commit

---
+++ src/main/java/com/example/test/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world     3333111";
+    }
+
+    private void test(){
+        new Date();
+    }
+
+}
